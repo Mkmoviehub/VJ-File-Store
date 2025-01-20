a03597f439ee8c62f488632b1b137e8c0f4a16bc
Mkmoviehub <kondapallishiva143@gmail.com>
2025-01-20 09:58:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,7 +15,7 @@ from plugins.database import get_file_details
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.errors import *
 from utils import verify_user, check_token, check_verification, get_token
-from config import AUTH_CHANNEL,  *
+from config import AUTH_CHANNEL,
 import re
 import json
 import base64
